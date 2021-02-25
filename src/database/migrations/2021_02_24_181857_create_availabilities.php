5266e1e6cd3bc910fafe8e201b9560e22a2d1984
Brad Griffith <brad@buckeyeinteractive.com>
2021-02-25 15:58:28 +0000

    Change availability table to plural availabilities

--- src/database/migrations/2021_02_24_181857_create_availabilities.php
+++ src/database/migrations/2021_02_24_181857_create_availabilities.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAvailability extends Migration
+class CreateAvailabilities extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAvailability extends Migration
      */
     public function up()
     {
-        Schema::create('availability', function (Blueprint $table) {
+        Schema::create('availabilities', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('location_id');
             $table->integer('doses');
@@ -30,6 +30,6 @@ class CreateAvailability extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('availability');
+        Schema::dropIfExists('availabilities');
     }
 }
