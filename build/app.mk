50d5decef12c016050e065eaf335d4b2cc1a5f04
Brad Griffith <brad@buckeyeinteractive.com>
2021-02-25 15:45:12 +0000

    Addd Mail From name and address

--- build/app.mk
+++ build/app.mk
@@ -1,4 +1,6 @@
 export REGISTRY_URL=683277130590.dkr.ecr.us-east-2.amazonaws.com
 export COMPOSE_PROJECT_NAME=vaccinateoh
 export APP_NAME=Vacciate Ohio
+export MAIL_FROM_NAME=Vaccinate Ohio
+export MAIL_FROM_ADDRESS=info@vaccinateoh.org
 PROD_WEB_SERVER=web1.vaccinateoh.org
