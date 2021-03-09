6286d4d706b115046c72d3b1b7c93f3aa3c2e797
Brad Griffith <brad@buckeyeinteractive.com>
2021-03-09 23:04:55 +0000

    Add soft deletes for Location and Availability models

--- src/app/Models/Availability.php
+++ src/app/Models/Availability.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Availability extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
+    use SoftDeletes;
 
     public $guarded = [];
 
--- src/app/Models/Location.php
+++ src/app/Models/Location.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use DB;
 
 use App\Helpers\Geo;
@@ -12,6 +13,7 @@ class Location extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
+    use SoftDeletes;
 
     public $appends = [
         'available'
