ff0c601260d1d60812838dee287af3229bb40ce3
Brad Griffith <brad@buckeyeinteractive.com>
2021-03-10 02:15:51 +0000

    Add a link in the admin sidebar to the hompage/map

--- src/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ src/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="/"><i class="la la-map-marked-alt nav-icon"></i> Home / Map</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-user'></i> Users</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon la la-map-marker'></i> Locations</a></li>
