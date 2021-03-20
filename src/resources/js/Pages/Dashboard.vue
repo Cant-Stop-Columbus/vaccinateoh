af78966ab92c37c7dd7c65ff74360aef3c5484ef
Brad Griffith <brad@buckeyeinteractive.com>
2021-03-20 13:26:34 +0000

    Add more styling to the stats dashboard

--- src/resources/js/Pages/Dashboard.vue
+++ src/resources/js/Pages/Dashboard.vue
@@ -1,22 +1,53 @@
 <template>
-    <h1 class="underline">Vaccinate Ohio Database Statistics:</h1>
-        Locations in the Database: {{ locationsCount }} <br>
-        Locations in the Database with Future Availability: {{ availableLocationsCount }} <br><br>
-    <h1 class="underline">Vaccinate Ohio Update Activity:</h1>
-        Updates in the last 24 hours: {{ last24Hrs }} <br>
-        Updates in the last 3 days: {{ last3Days }} <br>
-        Updates in the last week: {{ lastWeek }} <br><br>
-    <h1 class="underline">Top 10 Updaters:</h1>
-    <table>
-        <tr><td> Name </td><td> Updates </td></tr>
-        <tr v-for="updater in topUpdaters">
-            <td> {{updater.name}} </td><td> {{ updater.update_count }} </td>
-        </tr>
-    </table>
+    <div class="min-h-screen bg-gray-200">
+
+        <div class="container mx-auto">
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="py-6 px-4 sm:px-6 lg:px-8">
+                    <h1 class="text-2xl">VaccinateOH.org Stats</h1>
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main class="bg-white shadow py-6 px-4 sm:px-6 lg:px-8">
+                <h2 class="underline">Vaccinate Ohio Database Statistics:</h2>
+                    Locations in the Database: {{ locationsCount }} <br>
+                    Locations in the Database with Future Availability: {{ availableLocationsCount }} <br><br>
+                <h2 class="underline">Vaccinate Ohio Update Activity:</h2>
+                    Updates in the last 24 hours: {{ last24Hrs }} <br>
+                    Updates in the last 3 days: {{ last3Days }} <br>
+                    Updates in the last week: {{ lastWeek }} <br><br>
+                <h2 class="underline">Top Updaters:</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th class="text-left">Name</th>
+                            <th class="text-right">Updates</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="updater in topUpdaters">
+                            <td>{{updater.name}}</td>
+                            <td class="text-right">{{ updater.update_count }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
+        </div>
+    </div>
 </template>
 
+<style scoped>
+th,td {
+    padding: 5px;
+}
+tbody tr:nth-child(odd) {
+    background: #eee;
+}
+</style>
+
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
     import Welcome from '@/Jetstream/Welcome'
 
     export default {
@@ -29,8 +60,7 @@
             lastWeek: Number,
         },
         components: {
-            AppLayout,
-            Welcome,
+            Welcome
         },
     }
 </script>
