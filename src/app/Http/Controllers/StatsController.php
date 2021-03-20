a3542499f01bf69f73f011184d7ae011a222e6ad
Brad Griffith <brad@buckeyeinteractive.com>
2021-03-20 13:56:42 +0000

    Add top updaters today and last 7 days to the dashboard; more styling changes

--- src/app/Http/Controllers/StatsController.php
+++ src/app/Http/Controllers/StatsController.php
@@ -22,8 +22,11 @@ class StatsController extends Controller
         $locationsCount = Stats::countLocations();
         //Get the number of available locations with future availability from the DB
         $availableLocationsCount = Stats::countFutureAvailability();
-        //Get a list of the top 10 updaters.
-        $topUpdaters = Stats::topUpdaters(10);
+        //Get a list of the top updaters.
+        $topUpdaters = Stats::topUpdaters();
+        $topUpdatersToday = Stats::topUpdaters(-1, Carbon::today());
+        $topUpdaters7Days = Stats::topUpdaters(-1, Carbon::today()->subDays(6)); // today plus 6 days
+
         //Get the number of updates in the last 24 hours.
         $last24Hrs = Stats::updatesSince(Carbon::now()->subHours(24));
         //Get the number of updates in the last 3 days.
@@ -34,6 +37,8 @@ class StatsController extends Controller
           'locationsCount',
           'availableLocationsCount',
           'topUpdaters',
+          'topUpdatersToday',
+          'topUpdaters7Days',
           'last24Hrs',
           'last3Days',
           'lastWeek'
--- src/resources/js/Pages/Dashboard.vue
+++ src/resources/js/Pages/Dashboard.vue
@@ -5,34 +5,81 @@
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 class="text-2xl">VaccinateOH.org Stats</h1>
+                    <h1 class="text-2xl">VaccinateOH Stats</h1>
                 </div>
             </header>
 
             <!-- Page Content -->
             <main class="bg-white shadow py-6 px-4 sm:px-6 lg:px-8">
-                <h2 class="underline">Vaccinate Ohio Database Statistics:</h2>
-                    Locations in the Database: {{ locationsCount }} <br>
-                    Locations in the Database with Future Availability: {{ availableLocationsCount }} <br><br>
-                <h2 class="underline">Vaccinate Ohio Update Activity:</h2>
-                    Updates in the last 24 hours: {{ last24Hrs }} <br>
-                    Updates in the last 3 days: {{ last3Days }} <br>
-                    Updates in the last week: {{ lastWeek }} <br><br>
-                <h2 class="underline">Top Updaters:</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="text-left">Name</th>
-                            <th class="text-right">Updates</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="updater in topUpdaters">
-                            <td>{{updater.name}}</td>
-                            <td class="text-right">{{ updater.update_count }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="mb-6">
+                    <h2 class="underline text-lg">VaccinateOH Database Statistics:</h2>
+                    <div>Locations in the Database: {{ locationsCount }}</div>
+                    <div>Locations in the Database with Future Availability: {{ availableLocationsCount }}</div>
+                </div>
+                <div class="my-6">
+                    <h2 class="underline text-lg">VaccinateOH Update Activity:</h2>
+                    <div>Updates in the last 24 hours: {{ last24Hrs }}</div>
+                    <div>Updates in the last 3 days: {{ last3Days }}</div>
+                    <div>Updates in the last week: {{ lastWeek }}</div>
+                    <div class="italic text-sm">Note: The above stats include manual <strong>and</strong> automated updates</div>
+                </div>
+                
+                <div class="md:flex my-6">
+                    <div class="md:w-1/3">
+                        <h2 class="underline text-lg">Top Updaters (Today):</h2>
+                        <div v-if="!topUpdatersToday.length" class="italic">
+                            No updates yet :-(
+                        </div>
+                        <table v-else>
+                            <thead>
+                                <tr>
+                                    <th class="text-left">Name</th>
+                                    <th class="text-right">Updates</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="updater in topUpdatersToday">
+                                    <td>{{updater.name}}</td>
+                                    <td class="text-right">{{ updater.update_count }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="md:w-1/3">
+                        <h2 class="underline text-lg">Top Updaters (Last 7 Days):</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th class="text-left">Name</th>
+                                    <th class="text-right">Updates</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="updater in topUpdaters7Days">
+                                    <td>{{updater.name}}</td>
+                                    <td class="text-right">{{ updater.update_count }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="md:w-1/3">
+                        <h2 class="underline text-lg">Top Updaters (All Time):</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th class="text-left">Name</th>
+                                    <th class="text-right">Updates</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="updater in topUpdaters">
+                                    <td>{{updater.name}}</td>
+                                    <td class="text-right">{{ updater.update_count }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </main>
         </div>
     </div>
@@ -55,6 +102,8 @@ tbody tr:nth-child(odd) {
             locationsCount: Number,
             availableLocationsCount: Number,
             topUpdaters: Array,
+            topUpdatersToday: Array,
+            topUpdaters7Days: Array,
             last24Hrs: Number,
             last3Days: Number,
             lastWeek: Number,
