f85f63c9bcbf63408f00aff23f3098e8e4b89119
Will Hawkins <whh8b@obs.cr>
2021-03-27 02:35:17 -0400

    Add a histogram to the dashboard showing recency of provider updates
    Add a histogram to the dashboard showing recency of provider updates.

--- src/app/Helpers/Stats.php
+++ src/app/Helpers/Stats.php
@@ -24,19 +24,99 @@ class Stats {
     }
 
     /**
-     * Get the number of providers that have never had their data
+     * Get the number of locations that have never had their information
      * updated.
      */
-    public static function countNeverUpdated() {
-        return Location::whereNull('updated_at')->count();
+    public static function countLocationsNeverUpdated() {
+        return count(DB::select(DB::raw("select id from locations where id NOT IN (select location_id from availabilities group by location_id)")));
+    }
+
+    /**
+     * Get the number of providers whose most recent updates occurred longer
+     * than $days days ago.
+     *
+     * NB: This count will not include sites that have never been updated.
+     *
+     * @param days The cutoff from which to measure whether sites
+     * have been updated.
+     */
+    public static function countLocationsUpdatedLongerAgoThan(int $days) {
+        $results = Availability::where(DB::raw("floor((extract(epoch from NOW() - updated_at))/86400)"), ">", $days)
+          ->distinct()
+          ->count('location_id');
+        return $results;
+    }
+
+    /**
+     * Build an array of counts, where each index's count represents the number
+     * of providers updated at _index_ days in the past. For example, the array
+     * of counts that looks like
+     * 5, 6, 9, 1, 3
+     * means that 5 sites were last updated today, 6 were last updated
+     * yesterday, 9 where updated 2 days ago, 1 was updated 3 days ago and 3
+     * were updated 4 days ago.
+     *
+     * @param forDays The number of days that the histogram will contain.
+     */
+    public static function lastUpdatedHistogramData(int $forDays = 30) {
+        $buckets = [];
+        /**
+         * Count the number of sites updated during each of the last
+         * $forDays days.
+         */
+        $results = DB::select(DB::raw("select floor((extract(epoch from NOW() - updated_at))/86400) as since_update, count(*) from (select max(updated_at) as updated_at from availabilities group by location_id) as location_update_times where floor((extract(epoch from NOW() - updated_at))/86400) < ? group by since_update order by since_update asc"), [$forDays]);
+
+        /**
+         * Not every bucket will come back with a value. Therefore, we have
+         * to fill the missing buckets with 0s so that the graph will look nice.
+         * e.g., the buckets may be filled for index 0 (3), 4 (2) and 5 (7)
+         * but we want the array to look like:
+         * 3 0 0 0 4 7.
+         *
+         * Loop invariant: The index of the last bucket filled.
+         */
+        $priorBucketIndex = -1;
+        foreach ($results as $value) {
+            /**
+             * Fill in buckets where data is missing!
+             */
+            for ($missingIndex = $priorBucketIndex+1;
+                 $missingIndex <$value->since_update;
+                 $missingIndex++) {
+                $buckets[$missingIndex] = 0;
+            }
+            /**
+             * Deposit the bucket's value and maintain the loop invariant.
+             */
+            $buckets[$value->since_update] = intval($value->count);
+            $priorBucketIndex = $value->since_update;
+        }
+
+        /**
+         * Now, check to see whether we made enough buckets for the entire
+         * $forDays period.
+         */
+        for ($missingIndex = $priorBucketIndex+1;
+             $missingIndex<$forDays;
+             $missingIndex++) {
+            $buckets[$missingIndex] = 0;
+        }
+
+        /**
+         * Turn results into an array of arrays where each inner array contains
+         * the bucket label and the number of values in that bucket.
+         */
+        $results = array_map(function ($key) use ($buckets){ return [ $key, $buckets[$key] ]; }, array_keys($buckets));
+
+        return $results;
     }
 
     /**
      * Get a list of the top howMany most active updaters.
      *
      * @var howMany Specify the length of the top updaters list.
-		 *
-		 * TODO: Limit this by time.
+     *
+     * TODO: Limit this by time.
      *
      */
     public static function topUpdaters(int $howMany = -1, $start = null, $end = null) {
