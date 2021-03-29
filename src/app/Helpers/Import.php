20018caae8d256e317959196ab1834111ebf32b9
Brad Griffith <brad@buckeyeinteractive.com>
2021-03-29 03:46:35 +0000

    Move locations import from LocationController to the Import helper for easier testing and reusability

--- src/app/Helpers/Import.php
+++ src/app/Helpers/Import.php
@@ -3,15 +3,44 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Http;
-use App\Helpers\Address;
-use App\Models\Location;
 use Carbon\Carbon;
+use Session;
 use Storage;
 use Str;
 
+use App\Helpers\Address;
+use App\Models\AppointmentType;
+use App\Models\DataUpdateMethod;
+use App\Models\Location;
+use App\Models\LocationSource;
+use App\Models\LocationType;
+use App\Models\User;
+
+use Spatie\SimpleExcel\SimpleExcelReader;
+
 class Import 
 {
-    public static function getLatestFile($prefix, $all_states = false) {
+
+    const SPREADSHEET_HEADER_MAP = [
+        "Name" => "name",
+        "Address" => "address",
+        "County" => "county",
+        "Provider Phone" => "provider_phone",
+        "Booking Phone" => "phone",
+        "Provider URL" => "provider_url",
+        "Booking URL" => "bookinglink",
+        "Notes" => "siteinstructions",
+        "Provider Type" => "locationtype",
+        "System type" => "system_type",
+        "Primary update method" => "dataupdatemethod",
+        "Appointment Types" => "appointmenttypes",
+        "Location Data Source" => "locationsource",
+        "Data Collector Assigned" => "collectoruser",
+        "Scraper Developer" => -1,
+        "Scraper Status" => -1,
+    ];
+
+    public static function getLatestScrapedFile($prefix, $all_states = false) {
         $latest_file = collect(Storage::disk('s3')->files())
             ->filter(function($filename) use($prefix) {
                 return Str::startsWith($filename,$prefix);
@@ -32,7 +61,7 @@ class Import
     }
 
     public static function getMatchedLocations($prefix, $since = null, $one_match_only = true) {
-        $data = static::getLatestFile($prefix);
+        $data = static::getLatestScrapedFile($prefix);
 
         $vax_locations = collect();
         $data->each(function($vax_location) use(&$vax_locations) {
@@ -94,6 +123,191 @@ class Import
         return $ml->filter(function($l) {
             return count($l['location_matches']) == 0;
         })->pluck('address');
+    }
+
+    public static function getLatestLocationImportFile() {
+        $files = Storage::files('location_uploads');
+        $latest_modified_date = new Carbon('2000-01-01');
+        return collect($files)->reduce(function($latest, $file) use($latest_modified_date) {
+            $modified_date = new Carbon(Storage::lastModified($file));
+            if($latest_modified_date->lt($modified_date)) {
+                $latest_modified_date = $modified_date;
+                return $file;
+            } else {
+                return $latest;
+            }
+        });
+    }
+
+    public static function importMissingLocations($import_header_map, $import_path = null) {
+
+        $imported_data = static::processImportedFile($import_path);
+
+        $locations = [];
+        $errors = [];
+        foreach($imported_data['rows'] as $row) {
+            if($row['locations']->count() == 0) {
+                //get the values and do the import
+                $location = ['location' => []];
+                foreach($import_header_map as $imported_column => $field_name) {
+                    $field_value = $row['data'][$imported_column];
+                    if($field_name == '-1') { // skip columns with a mapped column of -1
+                    } else if($field_name == 'locationtype') {
+                        $lt = LocationType::where('short',substr($field_value,0,1))->first();
+                        if(!$lt) {
+                            $errors[] = "Location type $field_value not found";
+                            continue;
+                        }
+                        $location['location']['location_type_id'] = $lt->id;
+                    } else if($field_name == 'dataupdatemethod') {
+                        $dum = DataUpdateMethod::where('name',$field_value)->first();
+                        if(!$dum) {
+                            $errors[] = "Data Update Method $field_value not found";
+                            continue;
+                        }
+                        $location['location']['data_update_method_id'] = $dum->id;
+                    } else if($field_name == 'locationsource') {
+                        $ls = LocationSource::where('name',$field_value)->first();
+                        if(!$ls) {
+                            $errors[] = "Location Source $field_value not found";
+                            continue;
+                        }
+                        $location['location']['location_source_id'] = $ls->id;
+                    } else if($field_name == 'collectoruser') {
+                        $u = User::where('name',$field_value)->first();
+                        if(!$u) {
+                            $errors[] = "Data Collector User $field_value not found";
+                            continue;
+                        }
+                        $location['location']['collector_user_id'] = $u->id;
+                    } else if($field_name == 'appointmenttypes') {
+                        // remove spaces and split on commas
+                        $methods = explode(',',preg_replace('/\s+/', '', $field_value));
+                        $at = AppointmentType::whereIn('short',$methods)->pluck('id');
+                        if($at->count() != count($methods)) {
+                            $errors[] = "Invalid appointment type included in  $field_value";
+                            continue;
+                        }
+                        $location['appointmentTypes'] = $at;
+                    } else {
+                        $location['location'][$field_name] = $field_value;
+                    }
+                }
+                $locations[] = $location;
+            }
+        }
+
+        foreach($locations as $location) {
+            $l = Location::create($location['location']);
+            if(!empty($location['appointmentTypes'])) {
+                $l->appointmentTypes()->sync($location['appointmentTypes']);
+            }
+        }
+        
+        $processed_file = static::processImportedFile($import_path);
+        return array_merge($processed_file, compact(['errors']));
+    }
+
+    public static function processImportedFile($path = null) {
+        if(!$path && Session::has('import_path')) {
+            $path = Session::get('import_path');
+        }
+
+        $storage_path = storage_path("app/$path");
+
+        $headers_required = [
+            'name' => [
+            ],
+            'address' => [
+            ],
+        ];
+        $headers_optional = [
+            'county' => [
+            ],
+            'phone' => [
+                'phone',
+                'booking phone',
+            ],
+            'provider_phone' => [
+                'provider phone'
+            ],
+            'bookinglink' => [
+                'web site',
+                'booking url',
+                'booking link',
+            ],
+            'provider_url' => [
+                'provider url',
+                'provider link',
+            ],
+            'siteinstructions' => [
+                'notes',
+            ],
+            'system_type' => [
+                'system type',
+            ],
+            'locationtype' => [
+                'provider type',
+            ],
+            'dataupdatemethod' => [
+                'update method',
+                'primary update method',
+            ],
+            'locationsource' => [
+                'location data source',
+            ],
+            'appointmenttypes' => [
+                'appointment types',
+            ],
+            'collectoruser' => [
+                'data collector assigned',
+            ],
+        ];
+        $headers_all = [];
+        foreach(array_merge($headers_required, $headers_optional) as $header => $aliases) {
+            $headers_all[$header] = $header;
+            foreach($aliases as $alias) {
+                $headers_all[$alias] = $header;
+            }
+        }
+
+        $reader = SimpleExcelReader::create($storage_path);
+        $headers_imported = $reader->getHeaders();
+
+        $rows_imported = SimpleExcelReader::create($storage_path)
+            ->getRows();
+
+        $rows = [];
+        foreach($rows_imported as $row_key => $row) {
+            // Look for location(s); may return a location or an array of locations
+            $rows[] = [
+                'data' => $row,
+                'locations' => Location::findByImportRow($row),
+            ];
+        }
+
+        $summary = [
+            'No Match' => [
+                'i' => 0,
+                'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) == 0 ? 1 : 0; }),
+            ],
+            'One Match' => [
+                'i' => 1,
+                'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) == 1 ? 1 : 0; }),
+            ],
+            'Multiple Matches' => [
+                'i' => '>1',
+                'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) > 1 ? 1 : 0; }),
+            ],
+        ];
+
+        return compact([
+            'summary',
+            'headers_imported',
+            'headers_all',
+            'rows',
+        ]);
+
     }
 }
 
--- src/app/Http/Controllers/LocationController.php
+++ src/app/Http/Controllers/LocationController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Storage;
 use Inertia\Inertia;
 
+use App\Helpers\Import;
 use App\Models\AppointmentType;
 use App\Models\DataUpdateMethod;
 use App\Models\Location;
@@ -17,119 +17,17 @@ use Spatie\SimpleExcel\SimpleExcelReader;
 
 class LocationController extends Controller
 {
-    //
-
     public function uploadImportFile(Request $request) {
         // process uploaded file
         if($request->has('fileupload')) {
             $file = $request->file('fileupload');
             $filename = date('Y-m-d_H:i').'_'.$file->getClientOriginalName();
 
-            $path = 'app/'.$file->storeAs('location_uploads', $filename);
-            $storage_path = storage_path($path);
-            $request->session()->put('import_path', $storage_path);
+            $path = $file->storeAs('location_uploads', $filename);
+            $request->session()->put('import_path', $path);
         }
 
-        return $this->processImportedFile($request);
-    }
-
-    private function processImportedFile(Request $request) {
-        if($request->session()->get('import_path')) {
-            $storage_path = $request->session()->get('import_path');
-            $headers_required = [
-                'name' => [
-                ],
-                'address' => [
-                ],
-            ];
-            $headers_optional = [
-                'county' => [
-                ],
-                'phone' => [
-                    'phone',
-                    'booking phone',
-                ],
-                'provider_phone' => [
-                    'provider phone'
-                ],
-                'bookinglink' => [
-                    'web site',
-                    'booking url',
-                    'booking link',
-                ],
-                'provider_url' => [
-                    'provider url',
-                    'provider link',
-                ],
-                'siteinstructions' => [
-                    'notes',
-                ],
-                'system_type' => [
-                    'system type',
-                ],
-                'locationtype' => [
-                    'provider type',
-                ],
-                'dataupdatemethod' => [
-                    'update method',
-                    'primary update method',
-                ],
-                'locationsource' => [
-                    'location data source',
-                ],
-                'appointmenttypes' => [
-                    'appointment types',
-                ],
-                'collectoruser' => [
-                    'data collector assigned',
-                ],
-            ];
-            $headers_all = [];
-            foreach(array_merge($headers_required, $headers_optional) as $header => $aliases) {
-                $headers_all[$header] = $header;
-                foreach($aliases as $alias) {
-                    $headers_all[$alias] = $header;
-                }
-            }
-
-            $reader = SimpleExcelReader::create($storage_path);
-            $headers_imported = $reader->getHeaders();
-
-            $rows_imported = SimpleExcelReader::create($storage_path)
-                ->getRows();
-
-            $rows = [];
-            foreach($rows_imported as $row_key => $row) {
-                // Look for location(s); may return a location or an array of locations
-                $rows[] = [
-                    'data' => $row,
-                    'locations' => Location::findByImportRow($row),
-                ];
-            }
-            
-            $summary = [
-                'No Match' => [
-                    'i' => 0,
-                    'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) == 0 ? 1 : 0; }),
-                ],
-                'One Match' => [
-                    'i' => 1,
-                    'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) == 1 ? 1 : 0; }),
-                ],
-                'Multiple Matches' => [
-                    'i' => '>1',
-                    'count' => collect($rows)->reduce(function($total, $loc) { return $total += count($loc['locations']) > 1 ? 1 : 0; }),
-                ],
-            ];
-
-            return compact([
-                'summary',
-                'headers_imported',
-                'headers_all',
-                'rows',
-            ]);
-        }
-
+        return Import::processImportedFile();
     }
 
     protected function import(Request $request) {
@@ -138,51 +36,11 @@ class LocationController extends Controller
 
     protected function processImport(Request $request) {
         // insert missing rows
+        $errors = [];
         if($request->has('missing')) {
            $map = $request->input('import_header_map');
 
-           $imported_data = $this->processImportedFile($request);
-
-           $locations = [];
-           foreach($imported_data['rows'] as $row) {
-                if(count($row['locations']) == 0) {
-                    //get the values and do the import
-                    $location = ['location' => []];
-                    foreach($map as $imported_column => $field_name) {
-                        $field_value = $row['data'][$imported_column];
-                        if($field_name == '-1') { // skip columns with a mapped column of -1
-                        } else if($field_name == 'locationtype') {
-                            $location['location']['location_type_id'] = LocationType::where('short',substr($field_value,0,1))->first()->id;
-                        } else if($field_name == 'dataupdatemethod') {
-                            $location['location']['data_update_method_id'] = DataUpdateMethod::where('name',$field_value)->first()->id;
-                        } else if($field_name == 'locationsource') {
-                            $location['location']['location_source_id'] = LocationSource::where('name',$field_value)->first()->id;
-                        } else if($field_name == 'collectoruser') {
-                            $location['location']['collector_user_id'] = User::where('name',$field_value)->first()->id;
-                        } else if($field_name == 'appointmenttypes') {
-                            // remove spaces and split on commas
-                            $methods = explode(',',preg_replace('/\s+/', '', $field_value));
-                            $location['appointmentTypes'] = AppointmentType::whereIn('short',$methods)->pluck('id');
-                        } else {
-                            $location['location'][$field_name] = $field_value;
-                        }
-                    }
-                    $locations[] = $location;
-                }
-            }
-
-            foreach($locations as $location) {
-                $l = Location::create($location['location']);
-                if(!empty($location['appointmentTypes'])) {
-                    $location_type_ids = [];
-                    foreach($location['locationType'] as $lt_short) {
-                        $location_type_ids[] = LocationType::whereShort($lt_short)->first()->pluck('id');
-                    }
-                    $l->locationType()->sync($locationTypes);
-                }
-            }
+           return Import::importMissingLocations($map);
         }
-        
-        return $this->processImportedFile($request);
     }
 }
