d6fa8385e7435c7dce0f95233742eb41e1721bfd
Brad Griffith <brad@buckeyeinteractive.com>
2021-04-01 21:19:03 +0000

    Add armorvax API endpoint

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -122,4 +122,16 @@ class ApiController extends Controller
 
         return $availability;
     }
+
+    public function armorvax(Request $request) {
+        $data = $request->all();
+        $data_string = json_encode($data, JSON_PRETTY_PRINT);
+        $path = sprintf('armorvax/%s/u%d_%s.json', config('app.env'), $request->user()->id, date('Y-m-d_his'));
+
+        // Write the full JSON data to a file on S3
+        \Storage::disk('s3')->write($path, $data_string);
+
+        // echo back the same json that was posted
+        return response()->json($data);
+    }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ApiController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,14 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->post('/armorvax', function (Request $request) {
-    $data = $request->all();
-    $data_string = json_encode($data, JSON_PRETTY_PRINT);
-    $path = sprintf('armorvax/%s/u%d_%s.json', config('app.env'), $request->user()->id, date('Y-m-d_his'));
-
-    // Write the full JSON data to a file on S3
-    \Storage::disk('s3')->write($path, $data_string);
-
-    // echo back the same json that was posted
-    return response()->json($data);
-});
+Route::middleware('auth:sanctum')->post('/armorvax', [ApiController::class, 'armorvax']);
