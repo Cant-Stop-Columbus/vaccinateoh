0a5c8353f95ec7d0b3b72c93f3bab1232f4cc48c
Brad Griffith <brad@buckeyeinteractive.com>
2021-04-04 16:26:17 +0000

    Add Todo items with links to the dashboard

---
+++ src/resources/views/vendor/backpack/base/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends(backpack_view('blank'))
+
+@php
+    $widgets['before_content'][] = [
+        'type'        => 'jumbotron',
+        'heading'     => trans('backpack::base.welcome'),
+        //'content'     => trans('backpack::base.use_sidebar'),
+        //'button_link' => backpack_url('logout'),
+        //'button_text' => trans('backpack::base.logout'),
+    ];
+@endphp
+
+@section('content')
+<p>Total Locations: {{ \App\Models\Location::count() }}</p>
+<h2>Todo List</h2>
+<p><em>There's work to be done if any of these numbers is greater than 0. Consider the gauntlet thrown down.</em></p>
+<p>Missing County: <a href="{{ route('location.index', ['county' => '[0]']) }}">{{ \App\Models\Location::whereNull('county')->count() }}</a></p>
+<p>Missing Appointment Types: <a href="{{ route('location.index', ['appointment_type_id' => '[0]']) }}">{{ \App\Models\Location::has('appointmentTypes', '<', 1)->count() }}</a></p>
+<p>Missing Location Type: <a href="{{ route('location.index', ['location_type_id' => '[0]']) }}">{{ \App\Models\Location::whereNull('location_type_id')->count() }}</a></p>
+<p>Missing Collector User: <a href="{{ route('location.index', ['collector_user_id' => 0, 'data_update_method_id' => '[2,3,4]']) }}">{{ \App\Models\Location::whereNull('collector_user_id')->whereIn('data_update_method_id',[2,3,4])->count() }}</a></p>
+<p>Availability never updated: <a href="{{ route('location.index', ['never_updated' => true]) }}">{{ \App\Models\Location::withTrashed()->has('availabilities', '<', 1)->count() }}</a></p>
+@endsection
