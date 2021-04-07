2e8404bcb362008d05f32f374610dc65e76ba317
Brad Griffith <brad@buckeyeinteractive.com>
2021-04-07 17:54:13 +0000

    Missing quote

--- src/resources/views/vendor/backpack/base/dashboard.blade.php
+++ src/resources/views/vendor/backpack/base/dashboard.blade.php
@@ -17,7 +17,7 @@
 <p>Missing County: <a href="{{ route('location.index', ['county' => '[0]']) }}">{{ \App\Models\Location::whereNull('county')->count() }}</a></p>
 <p>Missing Appointment Types: <a href="{{ route('location.index', ['appointment_type_id' => '[0]']) }}">{{ \App\Models\Location::has('appointmentTypes', '<', 1)->count() }}</a></p>
 <p>Missing Location Type: <a href="{{ route('location.index', ['location_type_id' => '[0]']) }}">{{ \App\Models\Location::whereNull('location_type_id')->count() }}</a></p>
-<p>Missing Data Update Method: <a href="{{ route('location.index', [data_update_method_id' => '[0]']) }}">{{ \App\Models\Location::whereNull('data_update_method_id')->count() }}</a></p>
-<p>Missing Collector User (with scraped/API update method): <a href="{{ route('location.index', ['collector_user_id' => 0, 'data_update_method_id' => '[2,3,4]']) }}">{{ \App\Models\Location::whereNull('collector_user_id')->whereIn('data_update_method_id',[2,3,4])->count() }}</a></p>
+<p>Missing Data Update Method: <a href="{{ route('location.index', ['data_update_method_id' => '[0]']) }}">{{ \App\Models\Location::whereNull('data_update_method_id')->count() }}</a></p>
+<p>Missing Collector User (with non-scraped/API update method): <a href="{{ route('location.index', ['collector_user_id' => 0, 'data_update_method_id' => '[2,3,4]']) }}">{{ \App\Models\Location::whereNull('collector_user_id')->whereIn('data_update_method_id',[2,3,4])->count() }}</a></p>
 <p>Availability never updated: <a href="{{ route('location.index', ['never_updated' => true]) }}">{{ \App\Models\Location::withTrashed()->has('availabilities', '<', 1)->count() }}</a></p>
 @endsection
