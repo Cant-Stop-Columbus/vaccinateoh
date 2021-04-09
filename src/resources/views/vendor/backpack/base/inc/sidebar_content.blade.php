37db8de49beb4fcfd1a98579c229dde8b9b02461
Brad Griffith <brad@buckeyeinteractive.com>
2021-04-09 02:05:00 +0000

    Add Priority Locations admin sidebar menu item

--- src/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ src/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,7 +2,11 @@
 <li class="nav-item"><a class="nav-link" href="/"><i class="la la-map-marked-alt nav-icon"></i> Home / Map</a></li>
 <hr>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-user'></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ route('location.index', [
+    'location_source_id' => '["0","1"]',
+    'location_type_id' => '["1","3"]',
+    'data_update_method_id' => '["2","3","4"]',
+]) }}'><i class='nav-icon la la-exclamation-triangle'></i> Priority Locations</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon la la-map-marker'></i> Locations</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('availability') }}'><i class='nav-icon la la-syringe'></i> Availabilities</a></li>
 <hr>
